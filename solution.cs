bf2a363bb6c416a70dfd45309dc6c649dd79b7ff
Kooreis <kooreis.official@gmail.com>
2022-12-08 02:47:31 -0600

    Iterates over each string in the input array, sorts the characters in the string, and uses the sorted characters as a key in the map. If the key does not exist, it is created with an empty array as its value. The string is then added to the array of the corresponding key.

--- solution.cs
+++ solution.cs
@@ -1,3 +1,5 @@
-function groupAnagrams(strs) {
-    let map = new Map();
-}
+for(let str of strs) {
+        let characters = Array.from(str).sort().join('');
+        if(!map.has(characters)) map.set(characters, []);
+        map.get(characters).push(str);
+    }
