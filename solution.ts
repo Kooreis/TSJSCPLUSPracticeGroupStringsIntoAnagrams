eaa861f4efd6644b78ade74493fecd2ca69a0c7a
Kooreis <kooreis.official@gmail.com>
2022-12-08 02:47:31 -0600

    The second part of the script adds a public static method 'groupAnagrams' to the 'AnagramGrouper' class. This method takes an array of words as input and groups them into anagrams. It does this by creating an object where the keys are sorted words and the values are arrays of words that are anagrams of the key. The method then returns the values of the object, which are the groups of anagrams.

--- solution.ts
+++ solution.ts
@@ -1,5 +1,15 @@
-class AnagramGrouper {
-    private static sortWord(word: string): string {
-        return word.split('').sort().join('');
+public static groupAnagrams(words: string[]): string[][] {
+        const anagramGroups: { [key: string]: string[] } = {};
+
+        for (const word of words) {
+            const sortedWord = this.sortWord(word);
+            if (anagramGroups[sortedWord]) {
+                anagramGroups[sortedWord].push(word);
+            } else {
+                anagramGroups[sortedWord] = [word];
+            }
+        }
+
+        return Object.values(anagramGroups);
     }
 }
